890e546edb8b25861d57a0692a0bc677646d2bcc
ritikkalal12 <104815321+ritikkalal12@users.noreply.github.com>
2025-12-11 18:38:42 +0530

    Added origins in server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB();
 await connectCloudinary();
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', ''];
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-er7g.vercel.app'];
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
@@ -39,3 +39,4 @@ app.use('/api/order', orderRouter);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
